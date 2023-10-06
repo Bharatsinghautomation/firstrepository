0781aacb64474c7f89229869aee21f7adcd589e3
Bharatsinghautomation <136411299+Bharatsinghautomation@users.noreply.github.com>
2023-10-06 22:22:25 +0530

    first.commit...

---
+++ src/septbatch/Methodoverloading2.java
@@ -0,0 +1,16 @@
+package septbatch;
+
+public class Methodoverloading2 {
+
+	
+	void loginpage ()
+	{
+		 System.out.println("login is  successfull");
+		 
+	}
+	
+	void loginpage (String name )
+	{
+		System.out.println("login with username is successfull");
+	}
+}
